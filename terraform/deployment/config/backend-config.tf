d91462735847bc02c2bf9ce036932fd73b2fc7fe
EricHasegawa <erichasegawa1@gmail.com>
2023-04-18 16:34:25 -0700

    starting fargate deployment work

---
+++ terraform/deployment/config/backend-config.tf
@@ -0,0 +1,5 @@
+bucket                      = "web-terraform-states"
+key                         = "tf-states"
+region                      = "us-west-1"
+encrypt                     = true
+dynamodb_table              = "web-terraform-locks"
